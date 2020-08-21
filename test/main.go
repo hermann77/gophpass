c1db459cad29413edd3a24b52c5427b2e290ce44
Hermann Schwarz <tux1977@gmx.de>
2020-08-21 22:42:13 +0200

    test main

---
+++ test/main.go
@@ -0,0 +1,26 @@
+package main
+
+
+import (
+	"github.com/hermann77/gophpass"
+)
+
+
+
+
+
+
+func main() {
+   
+	/*   
+	   hash, _ := HashedPassword("testPassword", 16)
+	   fmt.Printf("Hash: %s \n", hash)
+	   fmt.Printf("Hash-Length %d \n", len(hash))
+   */
+   
+	   Check("testPassword", "$S$Em2lMf9zE4rj0yyTNb3X5n7eyl/ST8aZ0lADIwlPOR5f.m9HhUxw")
+   
+	
+   
+   }
+   
