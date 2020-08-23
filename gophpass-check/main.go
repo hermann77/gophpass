a62ad9456bbce1acfeb174c02ac680ca0aff0261
Hermann Schwarz <tux1977@gmx.de>
2020-08-23 20:59:12 +0200

    more debugging output because named parameters don't work

--- gophpass-check/main.go
+++ gophpass-check/main.go
@@ -20,16 +20,22 @@ func main() {
 	if len(flag.Args()) > 1 {
 		if password == "" {
 			password = strings.Join(flag.Args()[1:], "")
+			fmt.Printf("password par: %s", password)
+		} else {
+			fmt.Printf("password NAMED par: %s", password)
 		}
 		if hash == "" {
 			hash = flag.Args()[1]
+			fmt.Printf("hash par: %s", hash)
+		} else {
+			fmt.Printf("hash NAMED par: %s", hash)
 		}
 	}
 
 	if gophpass.Check(password, hash) {
-		fmt.Printf("1. Password and Hash matched\n")
+		fmt.Printf("2. Password and Hash matched\n")
 	} else {
-		fmt.Printf("1. Password and Hash are NOT matched\n")
+		fmt.Printf("2. Password and Hash are NOT matched\n")
 		os.Exit(1)
 	}
 	 
