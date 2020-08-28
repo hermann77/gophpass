31c2b667509d64be8103fc14e27cbde05862c58d
Hermann Schwarz <tux1977@gmx.de>
2020-08-28 21:45:07 +0200

    Code cleanup

--- gophpass-check/main.go
+++ gophpass-check/main.go
@@ -4,22 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
-//	"strings"
-
 	"github.com/hermann77/gophpass"
 )
 
 func main() {
 
 	var password string
-//	var hash *string
 	var hash string
 
 	flag.StringVar(&password, "password", "", "password you want to check against a hash")
 	flag.StringVar(&hash, "hash", "", "Hash you want to validate")
 	flag.Parse()
-	
-	fmt.Printf("flagArgsLength: %d \n", len(flag.Args()))
 
 	fmt.Printf("password: %s\n", password)
 	fmt.Printf("hash: %s\n", hash)
@@ -36,9 +31,9 @@ func main() {
 	}
 
 	if gophpass.Check(password, hash) {
-		fmt.Printf("3. Password and Hash matched\n")
+		fmt.Printf("Password and Hash matched\n")
 	} else {
-		fmt.Printf("3. Password and Hash are NOT matched\n")
+		fmt.Printf("Password and Hash NOT matched\n")
 		os.Exit(1)
 	}
 	 
