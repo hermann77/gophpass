eed391ec61e042761251c102b1bda268eb74fff0
Hermann Schwarz <tux1977@gmx.de>
2020-08-28 21:39:53 +0200

    test with StringVar instead of String as command line parameter

--- gophpass-check/main.go
+++ gophpass-check/main.go
@@ -12,33 +12,30 @@ import (
 func main() {
 
 	var password string
-	var hash *string
+//	var hash *string
+	var hash string
 
 	flag.StringVar(&password, "password", "", "password you want to check against a hash")
-	hash = flag.String("hash", "", "Hash you want to validate")
+	flag.StringVar(&hash, "hash", "", "Hash you want to validate")
 	flag.Parse()
 	
 	fmt.Printf("flagArgsLength: %d \n", len(flag.Args()))
 
 	fmt.Printf("password: %s\n", password)
-	fmt.Printf("hash: %s\n", *hash)
+	fmt.Printf("hash: %s\n", hash)
 
 	if len(flag.Args()) > 1 {
 		if password == "" {
 			password = flag.Args()[0]
 			fmt.Printf("password par: %s\n", password)
-		} else {
-			fmt.Printf("password NAMED par: %s\n", password)
-		}
-		if *hash == "" {
-			hash = &flag.Args()[1]
-			fmt.Printf("hash par: %s\n", *hash)
-		} else {
-			fmt.Printf("hash NAMED par: %s\n", *hash)
-		}
+		} 
+		if hash == "" {
+			hash = flag.Args()[1]
+			fmt.Printf("hash par: %s\n", hash)
+		} 
 	}
 
-	if gophpass.Check(password, *hash) {
+	if gophpass.Check(password, hash) {
 		fmt.Printf("2. Password and Hash matched\n")
 	} else {
 		fmt.Printf("2. Password and Hash are NOT matched\n")
